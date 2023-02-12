50999e5e0dae41db126c523a3f175921d2f7572b
Meshak Otieno <meshakotieno343@gmail.com>
2023-02-13 01:17:52 +0300

    updated files

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 async function seed() {
   const john = await prisma.user.create({
     data: {
-      username: 'mesho',
+      username: 'john',
       // Hash for password - twixrox
       passwordHash:
         '$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u',
